5e18b68db07da797f6eed5224e162bf9b5f8453f
Dave <dave@webdave.de>
2023-11-09 09:11:49 +0100

    Communication between Components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabComponent } from '../shared/controls/tab/tab.component';
+import { TabbedPaneComponent } from '../shared/controls/tabbed-pane/tabbed-pane.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, TabComponent],
+  imports: [CommonModule, TabComponent, TabbedPaneComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
---
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -0,0 +1,32 @@
+import { AfterContentInit, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabbed-pane',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabbed-pane.component.html',
+  styleUrl: './tabbed-pane.component.css',
+})
+export class TabbedPaneComponent implements AfterContentInit {
+  tabs: Array<TabComponent> = [];
+  activeTab: TabComponent | undefined;
+
+  ngAfterContentInit(): void {
+    if (this.tabs.length > 0) {
+      this.activate(this.tabs[0]);
+    }
+  }
+
+  register(tab: TabComponent): void {
+    this.tabs.push(tab);
+  }
+
+  activate(active: TabComponent): void {
+    for (const tab of this.tabs) {
+      tab.visible = tab === active;
+    }
+    this.activeTab = active;
+  }
+}

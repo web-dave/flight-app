fdf7d79360bd7b33cbd32bfd34804be68c71601f
Dave <dave@webdave.de>
2023-11-09 09:03:40 +0100

    Content Projection

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabComponent } from '../shared/controls/tab/tab.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TabComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })

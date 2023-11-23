f94412915b63ca315f30f28021dbc5ffe6b23f1f
Dave <dave@webdave.de>
2023-11-23 10:07:38 +0100

    Routing demo

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-sidebar-cmp',
   templateUrl: 'sidebar.component.html',
+  imports: [RouterLink],
 })
 export class SidebarComponent {}
